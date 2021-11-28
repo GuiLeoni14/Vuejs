1f7072ef6968c93098ec4b00f8d0ecd7169cd847
GuiLeoni14 <guileone167@gmail.com>
2021-11-28 18:41:10 -0200

    Conhecendo as diretivas (v-if, v-show, v-for)

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,16 +1,24 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
-        <p>Utilizo as seguintes tecnologias:</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>Python</li>
             <li>JavaScript</li>
             <li>PHP</li>
         </ul>
+        <p v-show="mostrar_email">Mande seu email para: {{ email }}</p>
     </div>
 </template>
 <script>
 export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+        return {
+            esta_trabalhando: true,
+            mostrar_email: true,
+            email: 'teste@gmail.com'
+        }
+    },
 }
 </script>
