44ce868fd56747a1c784a324d96a37f9c3e2db29
GuiLeoni14 <guileone167@gmail.com>
2021-11-29 13:43:52 -0200

    Emit

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,9 +1,21 @@
 <template>
-    <img :src="imagem" :alt="descricao">
+    <div>
+        <img :src="imagem" :alt="descricao">
+        <MudarImagem @mudar-imagem="trocarImagem" />
+    </div>
 </template>
 <script>
+import MudarImagem from "./MudarImagem.vue"
 export default {
     name: 'Picture',
+    components:{
+        MudarImagem,
+    },
+    methods: {
+        trocarImagem(){
+            this.imagem = "/img/download.jpg"
+        }
+    },
     data() {
         return {
             imagem: '/img/imagem.jpg',
